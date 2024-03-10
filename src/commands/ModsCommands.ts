f97b23cfd5a02b3a59977580ba4252d992c7fbb5
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-03-10 15:52:30 +0100

    added second command for patreon only mods

--- src/commands/ModsCommands.ts
+++ src/commands/ModsCommands.ts
@@ -50,6 +50,48 @@ export class MemberCommands {
     }
 
     @SlashGroup("mods")
+    @Slash({ description: "Create a mod that can be encrypted later on only accessible by patreon role", name: "create-patreon" })
+    async create2(
+        @SlashOption({ description: "Name of the mod", name: "name", required: true, type: ApplicationCommandOptionType.String, }) name: string,
+        @SlashOption({ description: "Version of the mod", name: "version", required: true, type: ApplicationCommandOptionType.String, }) version: string,
+        @SlashOption({
+            description: "Filename of the mod (Example: ca_whiltonmill_2024.pkz)"
+            , name: "filename",
+            required: true,
+            type: ApplicationCommandOptionType.String,
+        }) filename: string,
+        interaction: CommandInteraction
+    ) {
+        const commandSender = await prisma.member.findUnique({
+            where: { discord: interaction.user.id }
+        })
+
+        // Check weather command sender has permissions or is admin
+        if (!commandSender || !commandSender.isAdmin && interaction.guild?.ownerId !== interaction.user.id) {
+            interaction.reply({ content: ":x: - You don't have permission to use this command", ephemeral: true });
+            return
+        }
+
+        try {
+            await prisma.mods.create({
+                data: {
+                    name,
+                    version,
+                    filename
+                }
+            })
+        } catch (err) {
+            interaction.reply({ content: ":x: - Internal issue with the database", ephemeral: true });
+            return
+        }
+
+        interaction.reply({
+            content: `:white_check_mark: - Successfully created mod with name ${name}, version ${version} and filename ${filename}`,
+            ephemeral: true
+        });
+    }
+
+    @SlashGroup("mods")
     @Slash({ description: "Remove a mod with his name", name: "remove" })
     async remove(
         @SlashOption({ description: "Name of the mod", name: "name", required: true, type: ApplicationCommandOptionType.String, }) name: string,
